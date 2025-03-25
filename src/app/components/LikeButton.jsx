d7f39c5d3f7979bafe095da48201327be7a939e6
Rasmus <rame0001@stud.kea.dk>
2025-03-25 11:54:42 +0100

    Wack

--- src/app/components/LikeButton.jsx
+++ src/app/components/LikeButton.jsx
@@ -1,14 +1,14 @@
 "use client";
 import { useState } from "react";
-import { AiOutlineLike } from "react-icons/ai";
+import { AiFillLike } from "react-icons/ai";
 
 const LikeButton = (props) => {
     const [likes, setLikes] = useState(0);
     return (
     <button onClick={() => setLikes(likes + 1)}>
-        <AiOutlineLike /> {props.label}
+        <AiFillLike /> {props.label}
         {likes > 1 ? "s" : ""} {likes}
-        
+
         </button>
         );
     };
